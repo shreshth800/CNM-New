cd580858da1fe47cab6f02f16e636009c684e4d2
shreshth800 <shreshthsharma800@gmail.com>
2024-08-01 20:46:32 +0530

    Folder Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import react from "react";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <>Hello</>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <img src="" alt="" />
+        <h1>Caterersnearme</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
