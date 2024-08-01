c8ed2f80863b93f7cc31906a935bb547a656500e
shreshth800 <shreshthsharma800@gmail.com>
2024-08-01 21:56:34 +0530

    Very basic Navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,38 @@
+// import React from "react";
+// import styles from "./Header.module.css";
+// import CNMLogo from "../../assets/CNMLogo-NoBG.png";
+
+// const Header = () => {
+//   return (
+//     <nav className={styles.navbar}>
+//       <div className={styles.logo}>
+//         <img className={styles.logoImg} src={CNMLogo} alt="CNM" />
+//         <h1>Caterersnearme</h1>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Header;
+
 import React from "react";
 import styles from "./Header.module.css";
+import CNMLogo from "../../assets/CNMLogo-NoBG.png";
 
 const Header = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>
-        <img src="" alt="" />
-        <h1>Caterersnearme</h1>
+        <div className={styles.imgContainer}>
+          <img className={styles.logoImg} src={CNMLogo} alt="CNM" />
+        </div>
+        <h1 className={styles.logoText}>Caterersnearme</h1>
       </div>
+      <ul className={styles.navLinks}>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
     </nav>
   );
 };
