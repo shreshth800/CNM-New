362aaeb09e32ef7d49bdfa1ee44e1a333fbc15b1
Anurag_Mishra <105621998+Anuragm473@users.noreply.github.com>
2024-08-03 13:23:44 +0000

    added our speciality section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HowItWorks from "../../components/HowItWorks/HowItWorks";
 import AboutUsSection from "../../components/AboutUsSection/AboutUsSection";
+import Ourspeciality from "../../components/Ourspeciality/Ourspeciality";
 const HomePage = () => {
   return (
     <>
       <AboutUsSection />
+      <Ourspeciality/>
       <HowItWorks />
     </>
   );
