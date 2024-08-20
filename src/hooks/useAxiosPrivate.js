56d15396c71503b2afae063ddfc0a5002e93d3c3
shreshth800 <shreshthsharma800@gmail.com>
2024-08-20 13:06:52 +0530

    Improved axios provider and minor functions

---
+++ src/hooks/useAxiosPrivate.js
@@ -0,0 +1,30 @@
+import { axiosPrivate } from "../api/axios";
+import { useEffect } from "react";
+import useAuth from "../hooks/useAuth";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthProvider";
+
+const useAxiosPrivate = () => {
+  const { user } = useContext(AuthContext);
+
+  console.log(user);
+  useEffect(() => {
+    const requestIntercept = axiosPrivate.interceptors.request.use(
+      (config) => {
+        if (!config.headers["Authorization"]) {
+          console.log(user?.token);
+          //console.log(user);
+          config.headers["Authorization"] = `Bearer ${user?.token}`;
+        }
+        return config;
+      },
+      (error) => Promise.reject(error)
+    );
+
+    return () => {
+      axiosPrivate.interceptors.request.eject(requestIntercept);
+    };
+  }, [user, axiosPrivate]);
+  return axiosPrivate;
+};
+export default useAxiosPrivate;
