d193bbe9110dcbcf6ea88e4b669a50aa26eae93d
abhishek singh mahra <100080397+Namechangedd@users.noreply.github.com>
2024-09-11 09:50:07 +0530

    refixed the code with anurag branch

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header/Header";
 import { ToastContainer } from "react-toastify";
+import Footer1 from "./Footer1/Footer1"
 
 export default function AppLayout() {
   return (
@@ -8,6 +9,7 @@ export default function AppLayout() {
       <ToastContainer />
       <Header />
       <Outlet />
+      <Footer1 />
     </main>
   );
 }
